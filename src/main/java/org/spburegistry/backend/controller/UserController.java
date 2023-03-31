701f25f88bdf99c7b6d75aeff926892de496c0da
Khabi <khabimfw@gmail.com>
2023-03-31 18:19:43 +0300

    swagger 3.0.0 integrated

--- src/main/java/org/spburegistry/backend/controller/UserController.java
+++ src/main/java/org/spburegistry/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.spburegistry.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.spburegistry.backend.entity.User;
 import org.spburegistry.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,13 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users/user")
+    @Operation(description = "Get user by ID")
     public User getUser(@RequestParam() Long id) {
         return userService.findById(id);
     }
 
     @GetMapping("/users")
+    @Operation(description = "Get all users")
     public Iterable<User> getAllUsers() {
         return userService.findAll();
     }
