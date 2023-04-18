8dd1d897081052b97bb5e5a01e623850e29f704a
End-rey <andreyis2017@gmail.com>
2023-04-18 14:21:25 +0300

    fix pr

--- src/main/java/org/spburegistry/backend/controller/TagController.java
+++ src/main/java/org/spburegistry/backend/controller/TagController.java
@@ -1,5 +1,7 @@
 package org.spburegistry.backend.controller;
 
+import java.util.Optional;
+
 import org.spburegistry.backend.dto.TagTO;
 import org.spburegistry.backend.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RequestMapping("/data/tags")
 @RestController
@@ -17,25 +19,17 @@ public class TagController {
 
     @Autowired
     private TagService tagService;
-    
-    @GetMapping
-    public Iterable<TagTO> getAllTags() {
-        return tagService.findAll();
-    }
 
-    @GetMapping("/sortedByWeight")
-    public Iterable<TagTO> getAllTagsSortedByWeight() {
-        return tagService.findAllTagsSortedByWeight();
+    @GetMapping
+    public Iterable<TagTO> getTagsBySubstringSrtedByWeight(
+            @RequestParam("sortedByWeight") Optional<Boolean> sortedByWeight,
+            @RequestParam("substring") Optional<String> substring) {
+        return tagService.findTagsBySubstringSortedByWeight(sortedByWeight, substring);
     }
 
     @GetMapping("/tag/id/{id}")
     public TagTO getTagById(@PathVariable Long id) {
         return tagService.findTagById(id);
-    }
-
-    @GetMapping("/tag/substring/{substring}")
-    public Iterable<TagTO> getTagsBySubstring(@PathVariable String substring) {
-        return tagService.findTagBySubstring(substring);
     }
 
     @PostMapping("/tag")
